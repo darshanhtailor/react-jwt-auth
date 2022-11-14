a75c5133852231e2059b873cf13da1d4a0d49cdd
Rashid Aziz <md.r.a.n.786@gmail.com>
2022-11-14 20:51:05 +0530

    backend added

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const url = "mongodb+srv://alivewalker:12345@cluster0.russlnx.mongodb.net/inotebook?retryWrites=true&w=majority";
 
+
 const connectToAtlas = () => {
     mongoose.connect(url, {
         useNewUrlParser: true,
